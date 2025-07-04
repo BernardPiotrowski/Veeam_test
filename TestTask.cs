8f00d881dd0e2f93d9f6f62fd3b43512fc7e5afd
Bernard <bernard.piotrowski@gmail.com>
2025-07-05 00:09:43 +0200

    small cleaning

--- TestTask.cs
+++ TestTask.cs
@@ -11,12 +11,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 
-//using System.Threading;
 
-//using System.Threading;
-using System.Threading.Tasks;
-using System.Timers;
-//using static System.Net.WebRequestMethods;
 
 namespace Veeam_test
 {
